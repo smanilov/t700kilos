64178c3271e8b44cf8fb57d69874663d80e9c599
Stan Manilov <stanislav.manilov@gmail.com>
2021-06-10 19:03:27 +0200

    Fix merge conflicts and tests
    Forgot to add these as part of last commit.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,21 +5,24 @@ import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 
 import 'package:t700kilos/main.dart';
+import 'package:t700kilos/morning_evening_analysis.dart';
 import 'package:t700kilos/storage.dart';
 import 'package:t700kilos/clock.dart';
 import 'package:t700kilos/record.dart';
 
 import 'widget_test.mocks.dart';
 
-@GenerateMocks([Clock, Storage])
+@GenerateMocks([Clock, Storage, MorningEveningAnalyser])
 void main() {
   testWidgets('Submitting a record stores it', (WidgetTester tester) async {
     final storage = MockStorage();
     final clock = MockClock();
+    final analyser = MockMorningEveningAnalyser();
+
     when(clock.now).thenReturn(DateTime(1337));
 
     // Build our app and trigger a frame.
-    await tester.pumpWidget(T700KilosApp(storage, clock));
+    await tester.pumpWidget(T700KilosApp(storage, clock, analyser));
 
     // Verify that our welcome text is there.
     expect(find.text('Opa!'), findsOneWidget);
